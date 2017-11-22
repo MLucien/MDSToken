72e5643ca991bcc7cc7ac4eea6342485c28dc02e
Eric Yu <ericfish@gmail.com>
2017-11-22 20:48:54 +0800

    update gas and number

--- contracts/MDSToken.sol
+++ contracts/MDSToken.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.4.4;
 import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
 contract MDSToken is StandardToken {
-  string public name = 'MDSToken';
+  string public name = 'MDS Token';
   string public symbol = 'MDS';
   uint public decimals = 18;
-  uint public INITIAL_SUPPLY = 2000000000;
+  uint public INITIAL_SUPPLY = 2000000000000000000000000000;
 
   function MDSToken() {
     totalSupply = INITIAL_SUPPLY;
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var MDSToken = artifacts.require("./MDSToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MDSToken);
+  deployer.deploy(MDSToken, {gas: 5000000});
 };
