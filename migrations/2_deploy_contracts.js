530b544d46e90b735695394131f0f01739faf22e
Eric Yu <ericfish@gmail.com>
2017-11-22 22:34:45 +0800

    update migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+var Math = artifacts.require("./Math.sol");
 var MDSToken = artifacts.require("./MDSToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(MDSToken, {gas: 5000000});
+    deployer.deploy(Math, {gas: 6993115});
+    deployer.link(Math, [MDSToken]);
+    deployer.deploy(MDSToken, {gas: 5000000});
 };
