28ec6b16bb5ad7c3d50624deaf6fa3cc61dc7f43
Eric Yu <ericfish@gmail.com>
2017-11-23 10:24:47 +0800

    change to HumanStandardToken

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var Math = artifacts.require("./Math.sol");
-var MDSToken = artifacts.require("./MDSToken.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(Math, {gas: 6993115});
-    deployer.link(Math, [MDSToken]);
-    deployer.deploy(MDSToken, {gas: 5000000});
-};
--- util/util.js
+++
@@ -1,1 +0,0 @@
-var ethUtil = require('ethereumjs-util');
